f6011fe0352899282b06c0b32dc6113ca07eabc8
liuyaoCS <liuyao@chinaso.com>
2016-10-09 13:56:34 +0800

    加入信用贷

--- app/src/main/java/com/ly/housepay/MainActivity.java
+++ app/src/main/java/com/ly/housepay/MainActivity.java
@@ -11,10 +11,41 @@ public class MainActivity extends AppCompatActivity {
     EditText housePrice,houseEvaluation,charge,taxcharge,others;
     Button compute;
     TextView result;
+
+    EditText creditSum,creditRate,creditTime;
+    Button creditCompute;
+    TextView creditRet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initHouseFee();
+        initCreditLoan();
+    }
+
+    private void initCreditLoan() {
+        creditSum= (EditText) findViewById(R.id.creditSum);
+        creditRate= (EditText) findViewById(R.id.creditRate);
+        creditTime= (EditText) findViewById(R.id.creditTime);
+        creditCompute= (Button) findViewById(R.id.creditCompute);
+        creditRet= (TextView) findViewById(R.id.creditRet);
+        creditCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double sum=Double.parseDouble(creditSum.getText().toString());
+                double rate=Double.parseDouble(creditRate.getText().toString());
+                double time=Double.parseDouble(creditTime.getText().toString());
+
+                double interest=sum*10000*rate/100*time;
+                double monthPay=(sum*10000+interest)/(time*12);
+
+                creditRet.setText("利息总额->"+Math.ceil(interest)+" 元\n"
+                        +"每月还款->"+Math.ceil(monthPay)+" 元");
+            }
+        });
+    }
+
+    private void initHouseFee() {
         housePrice= (EditText) findViewById(R.id.housePrice);
         houseEvaluation= (EditText) findViewById(R.id.evaluation);
         charge= (EditText) findViewById(R.id.charge);
@@ -34,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
                 double dy=4.9*0.85/100;
                 double dm=dy/12;
-                double sum=hp*10000*he*0.7;
+                double sum=hp*10000*he*0.65;
                 int months=360;
                 double downPay=hp*10000-sum+hp*10000*ch/100+hp*10000*he*tch/100+other*10000;
                 double ret=sum*dm*Math.pow((1+dm), months);
@@ -47,6 +78,5 @@ public class MainActivity extends AppCompatActivity {
                         +"收入要求->"+Math.ceil(desiredIncome)+" 元");
             }
         });
-
     }
 }
